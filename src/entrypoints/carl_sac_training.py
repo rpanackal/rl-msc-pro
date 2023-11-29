38211525b65ee0173f8f513472e3a412b7e09097
Roshin Rajan Panackal <rajanro@informatik.uni-freiburg.de>
2023-11-29 13:35:56 +0100

    Fully Ready SAC with CARL Env Compatibility

--- src/entrypoints/carl_sac_training.py
+++ src/entrypoints/carl_sac_training.py
@@ -1,108 +1,20 @@
-import time
 from datetime import datetime
 from pathlib import PurePath
-from typing import Type, Union
 
-import carl
-import gym
-import gymnasium as gymz
-import numpy as np
 import torch
-from carl.envs import (CARLBraxHalfcheetah, CARLDmcQuadrupedEnv,
-                       CARLDmcWalkerEnv, CARLMountainCarContinuous,
-                       CARLPendulum)
-from carl.envs.dmc.carl_dmcontrol import CARLDmcEnv
-from gymnasium.wrappers import (FilterObservation, FlattenObservation,
-                                RecordEpisodeStatistics)
 from shimmy.openai_gym_compatibility import _convert_space
 from torch.utils.tensorboard import SummaryWriter
 
+from ..envs.core import setup_env
+from ..agents.adaptran import Adaptran
 from ..agents.carl_sac import CARLSACAgent
-from ..agents.adaptran_v2 import AdaptranV2
-from ..config import (BufferConfig, OptimizerConfig, ReinforcedLearnerConfig,
-                      SACAgentConfig)
-from ..envs.wrappers.compatibility import (EnvProjectCompatibility,
-                                           VecEnvProjectCompatibility)
-from ..envs.wrappers.normalization import RMVNormalizeVecObservation
+from ..config import (
+    BufferConfig,
+    OptimizerConfig,
+    ReinforcedLearnerConfig,
+    SACAgentConfig,
+)
 from ..utils import get_action_dim, get_observation_dim, set_torch_seed
-
-
-def setup_env(
-    env_cls_name,
-    n_envs=1,
-    seed=1,
-    env_suite="carl",
-    normalize_observation=False,
-    # normalize_kwargs=None,
-    render_mode=None,
-):
-    """Setup and return a vectorized environment."""
-
-    def make_env():
-        """Create and return an environment instance."""
-        if env_suite == "carl":
-            EnvCls: Type[
-                Union[
-                    CARLBraxHalfcheetah,
-                    CARLDmcQuadrupedEnv,
-                    CARLDmcWalkerEnv,
-                    CARLMountainCarContinuous,
-                    CARLPendulum,
-                ]
-            ] = eval(env_cls_name)
-
-            print("Default context", EnvCls.get_default_context())
-            # Just one context feature used
-            obs_context_features = [list(EnvCls.get_context_features().keys())[0]]
-
-            e: Union[
-                CARLBraxHalfcheetah,
-                CARLDmcQuadrupedEnv,
-                CARLDmcWalkerEnv,
-                CARLMountainCarContinuous,
-                CARLPendulum,
-            ] = EnvCls(
-                obs_context_as_dict=False
-            )  # obs_context_features=obs_context_features)
-
-            # When CARL observation and action space inconsistent
-            if isinstance(e.action_space, gym.Space) and isinstance(
-                e.observation_space, gymz.Space
-            ):
-                e.action_space = _update_action_space(e)
-
-            # * CARL SAC designed to handle Dict obs space
-            # e = RecordEpisodeStatistics(
-            #     FlattenObservation(FilterObservation(e, filter_keys=["obs"]))
-            # )
-        else:
-            e = gymz.make(env_cls_name, render_mode=render_mode)
-        
-        if hasattr(e, "action_space"):
-            e.action_space.seed(seed)
-        return EnvProjectCompatibility(e)
-
-    def _update_action_space(e):
-        """Update action space of the environment to gymnasium spaces."""
-        if isinstance(e, CARLDmcEnv):
-            action_spec = e.env.env.action_spec()
-            return gymz.spaces.Box(
-                action_spec.minimum, action_spec.maximum, dtype=action_spec.dtype
-            )
-
-        return _convert_space(e.action_space)
-
-    vec_env = gymz.vector.SyncVectorEnv([make_env for i in range(n_envs)])
-    vec_env = VecEnvProjectCompatibility(vec_env)
-    vec_env = RecordEpisodeStatistics(vec_env)
-    vec_env = gymz.wrappers.StepAPICompatibility(vec_env, output_truncation_bool=False)
-
-    # print("Seed: ", seed)
-    # vec_env.reset(seed=seed)
-    # vec_env.observation_space.seed(seed)
-    # vec_env.action_space.seed(seed)
-    return vec_env
-
 
 if __name__ == "__main__":
     config = ReinforcedLearnerConfig(
@@ -110,7 +22,7 @@ if __name__ == "__main__":
         batch_size=512,
         normalize_observation=False,
         agent=SACAgentConfig(
-            name="exp-adaptranv2",
+            name="CARLSAC",
             actor_optimizer=OptimizerConfig(lr=3e-4),
             critic_optimizer=OptimizerConfig(lr=3e-4),
             policy_frequency=32,
@@ -153,7 +65,7 @@ if __name__ == "__main__":
     with open(config_path, "w") as config_file:
         config_file.write(config.model_dump_json(exclude={"device"}))
 
-    agent = AdaptranV2(
+    agent = CARLSACAgent(
         envs=envs,
         critic_learning_rate=config.agent.critic_optimizer.lr,
         actor_learning_rate=config.agent.actor_optimizer.lr,
@@ -162,7 +74,7 @@ if __name__ == "__main__":
         writer=writer,
         log_freq=config.agent.log_freq,
         expanse_dim=config.agent.expanse_dim,
-        seed=config.random_seed
+        seed=config.random_seed,
     )
 
     agent.train(
@@ -179,4 +91,5 @@ if __name__ == "__main__":
 
     # agent.test(n_episodes=10)
     # agent.save_checkpoint(PurePath(writer.get_logdir()))
+    agent.replay_buffer.save(PurePath(writer.get_logdir()))
     envs.close()
